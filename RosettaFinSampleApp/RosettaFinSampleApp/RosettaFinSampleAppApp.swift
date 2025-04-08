cf7dc542d37fb2461c450cdc3952e8129846e991
Kenny Cabral <kenny@krui.me>
2025-04-08 06:50:16 -0400

    Initial Commit with project setup and running

---
+++ RosettaFinSampleApp/RosettaFinSampleApp/RosettaFinSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosettaFinSampleAppApp.swift
+//  RosettaFinSampleApp
+//
+//  Created by Kenny Cabral on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RosettaFinSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
